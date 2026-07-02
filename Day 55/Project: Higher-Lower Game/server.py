a0c916ab1efdca6cb13c35cb0c1a8dea3659d49e
Yuchen Liu <liuyushen123@gmail.com>
2026-07-01 23:22:25 -0400

    Day 55 finised

---
+++ Day 55/Project: Higher-Lower Game/server.py
@@ -0,0 +1,46 @@
+import random
+
+from flask import Flask
+
+random_number = random.randint(0, 9)
+app = Flask(__name__)
+
+
+def make_h1(function):
+    def wrapper():
+        return f"<h1>{function()}<h1>"
+
+    return wrapper
+
+
+@app.route("/")
+def home_page():
+    return (
+        "<h1>Guess a number between 0 and 9<h1>"
+        "<br><br>"
+        "<img src='https://media.giphy.com/media/3o7aCSPqXE5C6T8tBC/giphy.gif'>"
+    )
+
+
+@app.route("/<int:guess>")
+def guess_number(guess):
+    if guess > random_number:
+        return (
+            "<h1 style='color: purple'>Too high, try again!</h1>"
+            "<img src='https://media.giphy.com/media/3o6ZtaO9BZHcOjmErm/giphy.gif'/>"
+        )
+
+    elif guess < random_number:
+        return (
+            "<h1 style='color: red'>Too low, try again!</h1>"
+            "<img src='https://media.giphy.com/media/jD4DwBtqPXRXa/giphy.gif'/>"
+        )
+    else:
+        return (
+            "<h1 style='color: green'>You found me!</h1>"
+            "<img src='https://media.giphy.com/media/4T7e4DmcrP9du/giphy.gif'/>"
+        )
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
